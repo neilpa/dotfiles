cbfedda01550eaefd1b50f0a207822fdae5e3791
Neil Pankey <neilpa@live.com>
2014-01-12 12:52:20 -0800

    Run bootstrap from a different directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,9 @@
 #
 # Bootstrap the setup a new system on OS X.
 
+# Where is bootstrap
+DIR=$( cd "$( dirname "$0" )" && pwd )
+
 if [ ! $(command -v brew) ];
 then
   echo 'Installing Homebrew'
@@ -17,11 +20,11 @@ fi
 # Here down is effectively just update and should be runnable all the time
 
 # Install/Update everything in our Brewfile
-brew bundle
+brew bundle $DIR/Brewfile
 
 # Sync all the dotfile repositories
-mr -t -c mrconfig.symlink checkout
+mr -t -c $DIR/mrconfig.symlink checkout
 
 # Symlink files into the $HOME that need it
-./symlink.sh
+$DIR/symlink.sh
 
