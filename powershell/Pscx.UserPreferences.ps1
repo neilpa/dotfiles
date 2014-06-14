42dd97c4a17da893bb620021e45948ef13e4100a
Neil Pankey <neil@wire.im>
2014-06-14 16:45:01 -0700

    Remove windows stuff now that it has a branch

--- powershell/Pscx.UserPreferences.ps1
+++
@@ -1,8 +0,0 @@
-# Override some of the defaults when importing Pscx
-@{
-    CD_EchoNewLocation = $false
-    TextEditor = 'gvim.exe'
-    FileSizeInUnits = $true
-    ModulesToImport = @{ CD = $false }
-}
-
