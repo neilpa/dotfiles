e107a3cbc094f1690f2573c0e5f4238b1a2e148b
Neil Pankey <neil@wire.im>
2014-06-15 12:10:13 -0700

    Fix system to my liking

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,10 +1,3 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-if $(gls &>/dev/null)
-then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
-fi
+# Aliases for core system utilities
+alias la="ls -Alh"
+alias ll="ls -lh"
