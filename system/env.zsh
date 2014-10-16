d541d635efffd138057d3ab48ab91f37cab2ea0f
Neil Pankey <neil@wire.im>
2014-10-16 12:26:40 -0700

    Remove old zsh config

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,6 @@
 export EDITOR='vim'
+
+# Ensure XDG directories are defined
+[[ -n $XDG_CONFIG_HOME ]] || export XDG_CONFIG_HOME="$HOME/.config"
+[[ -n $XDG_CACHE_HOME ]] || export XDG_CACHE_HOME="$HOME/.cache"
+
