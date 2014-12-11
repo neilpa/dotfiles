94674acabfc12e21233f368bd804231942d3e9e4
Neil Pankey <npankey@gmail.com>
2014-12-11 14:57:47 -0800

    Add naven colorizer

---
+++ java/mvn.zsh
@@ -0,0 +1,26 @@
+# thanks to:  http://blog.blindgaenger.net/colorize_maven_output.html
+# and: http://johannes.jakeapp.com/blog/category/fun-with-linux/200901/maven-colorized
+# Colorize Maven Output
+alias maven="command mvn"
+function color_maven() {
+    local BLUE="[0;34m"
+    local RED="[0;31m"
+    local LIGHT_RED="[1;31m"
+    local LIGHT_GRAY="[0;37m"
+    local LIGHT_GREEN="[1;32m"
+    local LIGHT_BLUE="[1;34m"
+    local LIGHT_CYAN="[1;36m"
+    local YELLOW="[1;33m"
+    local WHITE="[1;37m"
+    local NO_COLOUR="[0m"
+    maven "$@" | sed \
+        -e "s/Tests run: \([^,]*\), Failures: \([^,]*\), Errors: \([^,]*\), Skipped: \([^,]*\)/${LIGHT_GREEN}Tests run: \1$NO_COLOUR, Failures: $RED\2$NO_COLOUR, Errors: $YELLOW\3$NO_COLOUR, Skipped: $LIGHT_BLUE\4$NO_COLOUR/g" \
+        -e "s/\(\[\{0,1\}WARN\(ING\)\{0,1\}\]\{0,1\}.*\)/$YELLOW\1$NO_COLOUR/g" \
+        -e "s/\(\[ERROR\].*\)/$RED\1$NO_COLOUR/g" \
+        -e "s/\(\(BUILD \)\{0,1\}FAILURE.*\)/$RED\1$NO_COLOUR/g" \
+        -e "s/\(\(BUILD \)\{0,1\}SUCCESS.*\)/$LIGHT_GREEN\1$NO_COLOUR/g" \
+        -e "s/\(\[INFO\].*\)/$LIGHT_GRAY\1$NO_COLOUR/g"
+    return $PIPESTATUS
+}
+
+alias mvn=color_maven
