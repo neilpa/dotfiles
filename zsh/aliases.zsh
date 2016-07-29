203f1542b0e0c7eb699fcc54a7ca08d2888c0aee
Neil Pankey <npankey@gmail.com>
2016-07-29 10:28:48 -0700

    Multi-move helper function

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,8 @@
 alias reload!='. ~/.zshrc'
+alias errcho='>&2 echo'
+
+# From the zmv usage
+# If you combine this with noglob, you don't even need to quote the arguments
+alias mmv='noglob zmv -W'
+# mmv *.c.orig orig/*.c" >&2
+
