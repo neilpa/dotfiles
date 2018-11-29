a4e1810ee013c2220a281aed33eaaf971497a732
Neil Pankey <me@neilpa.me>
2018-11-28 21:29:21 -0500

    bootstrap: skip already linked files

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,11 @@ set -eu
 cd "${HOME}/.config/tilde"
 for f in *
 do
-    ln -i "$f" "${HOME}/.$f"
+    # Test for existing hardlinks
+    if [ "$f" -ef "${HOME}/.$f" ]
+    then
+	echo "$f": already hardlinked
+    else
+        ln -vi "$f" "${HOME}/.$f"
+    fi
 done
