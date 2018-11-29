4e67bc36abc9897b94ff4eb501aec0283c295f46
Neil Pankey <me@neilpa.me>
2018-11-28 20:57:12 -0500

    st: slightly better font

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Nimbus Mono L:style=Regular:size=12";
 static int borderpx = 2;
 
 /*
