1059dc01fd5c65fced4a98904f4a0d86f25b71b3
Neil Pankey <x@neilpa.me>
2018-12-02 21:50:05 -0500

    tilde => _tilde~

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -eu
 
-cd "${HOME}/.config/tilde"
+cd "${HOME}/.config/_tilde"
 for f in *
 do
     # Test for existing hardlinks
     if [ "$f" -ef "${HOME}/.$f" ]
     then
-	echo "$f": already hardlinked
+	    echo "$f": already hardlinked
     else
         ln -vi "$f" "${HOME}/.$f"
     fi
