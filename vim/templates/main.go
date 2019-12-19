e433143149104d0caead2b17048b5e705326fa79
Neil Pankey <npankey@gmail.com>
2019-12-19 13:48:48 -0800

    vim: Expand main.go template

--- vim/templates/main.go
+++ vim/templates/main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+)
+
+var (
+	dbg = flag.Bool("dbg", false, "print debug spew")
+	out = flag.String("out", "output.ext", "output help message")
 )
 
 func main() {
-	fmt.Println("Hello World")
+	flag.Parse()
+	if flag.NArg() == 0 || *out == "" {
+		fmt.Fprintf(os.Stderr, "usage: %s [OPTIONS] <INPUT>...\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+
+	for _, arg := range flag.Args() {
+		debug("%s", arg)
+	}
+}
+
+
+func debug(format string, head interface{}, tail ...interface{}) {
+	if !*dbg {
+		return
+	}
+	format = os.Args[0] + ": " + format + "\n"
+	args := make([]interface{}, 1, len(tail) + 1)
+	args[0] = head
+	args = append(args, tail...)
+	fmt.Fprintf(os.Stderr, format, args...)
 }
